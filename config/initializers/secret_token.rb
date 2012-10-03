828a83ec0e93cd48790a91e82e4ef81c9c7858d3
Raymond Alvarez <nextwaveray@gmail.com>
2012-10-03 09:02:19 -0600

    add rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = '6763eed9404837b397c46364bbe4833f0489894e90a4fb7d597d29f24b4fc5fa4e557f14f3e09019949b94bd9401ec5955a6c99e417b996d4fc285a0a5a59c2d'
